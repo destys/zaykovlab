2848c59cd1e9a5daa1fadfed2e18df3cabe59ba1
destys <desstyss@gmail.com>
2024-08-16 12:06:38 +0300

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "favicon.svg",
+        "favicon.ico",
+        "robots.txt",
+        "apple-touch-icon.png",
+      ],
+      manifest: {
+        name: "ZaykobLab",
+        short_name: "ZaykobLab",
+        description: "ZaykobLab application",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "logo-stroke.svg",
+            sizes: "192x192",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
